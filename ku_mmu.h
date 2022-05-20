180a72839e9a29625de14dfd4452659b2f1ee66c
pil <44938522+hanu9257@users.noreply.github.com>
2022-05-21 01:07:03 +0900

    Feat : ku_page_fault skeleton

--- ku_mmu.h
+++ ku_mmu.h
@@ -37,8 +37,8 @@ struct linked_list *PCB_list = &linked_list;
 PCB *tempPCB;
 
 // TODO pmem : free list, allocated queue, swap : free list
-int pmem_cnt = 0; 
-int swap_cnt = 0;
+int pmem_index = 0; 
+int swap_index = 0;
 
 void insertPCB(PCB* input) {
     if(PCB_list -> head == NULL)
@@ -61,8 +61,25 @@ PCB *searchPCB(char searching_pid) {
     return NULL;
 }
 
+
 void *pmem = NULL;
 void *swap = NULL;
+
+int evict() {
+    return 0;
+}
+
+int swap_out() {
+    return 0;
+}
+
+int swap_in(ku_pte *accesed_pte) {
+    return 0;
+}
+
+int map_on_pmem(ku_pte *accesed_pte) {
+    return 0;
+}
 
 // 메모리와 스왑 공간 동적할당
 void *ku_mmu_init(unsigned int pmem_size, unsigned int swap_size) {
@@ -91,6 +108,8 @@ int ku_run_proc(char fpid, void **ku_cr3) {
     return 0;
 }
 
+int pmem_full_flag = 0;
+
 // TODO 1. PTE did not mapped
 // TODO 2. swaped out?? or map new PTE?? Both case needs to consider free space in swap and pmem
 // TODO 3. 해당 함수가 호출되면 이후 ku_trav()가 실행될 때 address translation 성공 필요
@@ -100,12 +119,24 @@ int ku_page_fault(char pid, char va) {
     ku_pte *target_pte = page_table+pte_offset;
     if(target_pte -> PFN == 0x0 && target_pte -> unused_bit == 0x0) {
         // TODO : Demand paging
-        /*  1. pmem에 빈 자리가 있는지 확인
-            2. 빈 자리가 없을 경우 avict() -> pmem_cnt가 끝까지 찼을 경우(왜냐하면 프로세스가 종료되지 않음 -> 이게 골때리네 free list가 굳이 필요가 없구나)
-            3. 빈 자리가 있을 경우 그냥 넣기 -> pmem_cnt가 끝까지 차지 않았을 경우 */
+        /*  
+        1. pmem에 빈 자리가 있는지 확인
+        2. 빈 자리가 없을 경우 avict() -> pmem_cnt가 끝까지 찼을 경우(왜냐하면 프로세스가 종료되지 않음 -> 이게 골때리네 free list가 굳이 필요가 없구나)
+        3. 빈 자리가 있을 경우 그냥 넣기 -> pmem_cnt가 끝까지 차지 않았을 경우 
+        */
+        if(pmem_full_flag) {
+            evict();
+            map_on_pmem(target_pte);
+        } else {
+            map_on_pmem(target_pte);
+        }    
     } else {
         // TODO : swap in
-        /* swap이 발생했으므로 pmem에는 빈 자리가 존재할 수 없음 -> pmem_cnt를 통해 FIFO avict */
+        swap_out();
+        swap_in(target_pte);
+        /* 
+        swap이 발생했으므로 pmem에는 빈 자리가 존재할 수 없음 -> pmem_cnt를 통해 FIFO avict 
+        */
     }
     return 0;
 }
