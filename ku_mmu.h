349e266e624936e5d92b7cbdcbcf6f44de14c8ac
pil <44938522+hanu9257@users.noreply.github.com>
2022-06-01 22:17:48 +0900

    Test : Clear about free&alloc list
    Refactor : Editing free&alloc list

--- ku_mmu.h
+++ ku_mmu.h
@@ -3,25 +3,26 @@
 #define PFN_MASK 0xFC
 #pragma pack(1)
 
-/*
-If every bit is 0 then it is unmapped 
-6bit PFN
-1bit unused bit
-1bit present bit
-*/
-typedef struct ku_pte {
+typedef struct linked_list;
+typedef struct ku_pte;
+typedef struct PCB;
+typedef struct node;
+typedef struct space;
+void insertPCB(struct PCB *input);
+struct PCB *searchPCB(char searching_pid);
+int swap_out();
+int swap_in(struct ku_pte *accesed_pte);
+int map_on_pmem(struct ku_pte *accesed_pte);
+
+typedef struct ku_pte
+{
     unsigned int present_bit : 1;
     unsigned int unused_bit : 1;
     unsigned int PFN : 6;
 } ku_pte;
 
-/*
-char pid
-void* PRBR
-PCB *prev
-PCB *next
-*/
-typedef struct PCB {
+typedef struct PCB
+{
     char pid;
     ku_pte *PTBR;
     struct PCB *prev;
@@ -29,32 +30,27 @@ typedef struct PCB {
 } PCB;
 
 // free list + alloc list node 개수의 합은 PFN 개수와 동일
-typedef struct node {
+typedef struct node
+{
     // char pid;
     ku_pte *rmap;
-    unsigned int prev_index;
-    unsigned int next_index;
+    struct node *prev;
+    struct node *next;
 } node;
 
-/*
-int free_head;
-int alloc_head;
-void *alloc_area;
-node *free_and_alloc;
-unsigned int full : 1;f
- */
-typedef struct space {
-    void *alloc_area;
-    node *free_and_alloc;
-    unsigned int free_head;
-    unsigned int free_tail;
-    unsigned int alloc_head;
-    unsigned int alloc_tail;
-} space;
 
-typedef struct linked_list {
-    PCB* head;
+typedef struct linked_list
+{
+    void *head; /* recent */
+    void *tail; /* old */
 } linked_list;
+
+typedef struct space
+{
+    void *alloc_area;
+    struct linked_list free_list;
+    struct linked_list alloc_list;
+} space;
 
 space ku_mmu_swap, ku_mmu_pmem;
 
@@ -63,121 +59,126 @@ linked_list *PCB_list_ptr = &PCB_list;
 
 PCB *tempPCB;
 
-void insertPCB(PCB * input);
-PCB *searchPCB(char searching_pid);
-int evict();
-int swap_out();
-int swap_in(ku_pte *accesed_pte);
-int map_on_pmem(ku_pte *accesed_pte);
-
-void insertPCB(PCB* input) {
-    if(PCB_list_ptr -> head == NULL)
-        PCB_list_ptr -> head = input;
-    else {
-        PCB_list_ptr -> head -> next = input;
-        input -> prev = PCB_list_ptr -> head;
+/**
+ * @brief Insert new process's PCB to PCB list.
+ *
+ * @param input New Process's PCB wants to insert.
+ */
+void insertPCB(PCB *input)
+{
+    if (PCB_list_ptr->head == NULL)
+        PCB_list_ptr->head = input;
+    else
+    {
+        ((PCB *)(PCB_list_ptr->head))->next = input;
+        input->prev = PCB_list_ptr->head;
     }
-    PCB_list_ptr -> head = input;
+    PCB_list_ptr->head = input;
 }
 
-PCB *searchPCB(char searching_pid) {
-    tempPCB = PCB_list_ptr -> head;
-    while(tempPCB != NULL) {
-        if(tempPCB -> pid == searching_pid)
+/**
+ * @brief Search given process's PCB in PCB list.
+ *
+ * @param searching_pid Process's ID wants to search.
+ * @return PCB* Searching process's PCB.
+ */
+PCB *searchPCB(char searching_pid)
+{
+    tempPCB = PCB_list_ptr->head;
+    while (tempPCB != NULL)
+    {
+        if (tempPCB->pid == searching_pid)
             return tempPCB;
         else
-            tempPCB = tempPCB -> prev;
+            tempPCB = tempPCB->prev;
     }
     return NULL;
 }
 
 /**
- * @brief 
- * 
+ * @brief Swap out pte to swap space by FIFO.
+ *
  * @return 0 on success, -1 on error.
  */
-int swap_out() {
+int swap_out()
+{
     /* pmem_index에 매핑되어있는 pte를 swap out시킴 */
-    unsigned int swap_dest = ku_mmu_swap.free_head;
-    if(!swap_dest) return -1; // swap_dest의 index가 0일 경우 == swap이 꽉 차있을 경우
+    unsigned int swap_dest;
+    if (!swap_dest)
+        return -1; // swap_dest의 index가 0일 경우 == swap이 꽉 차있을 경우
     /* swap space 값 변경 */
-    ku_mmu_swap.alloc_head = ku_mmu_swap.free_head;
-    ku_mmu_swap.free_head = ku_mmu_swap.free_and_alloc[ku_mmu_swap.free_head].next_index;
-    
-    /* swap out된 pte 값 변경 */
-    ku_pte *swap_out_pte = ku_mmu_pmem.free_and_alloc[ku_mmu_pmem.alloc_tail].rmap;
-    if(!swap_out_pte) return -1;
-    swap_out_pte -> PFN = (swap_dest&PTE_MASK)>>1;
-    swap_out_pte -> unused_bit = swap_dest&0x1;
-    swap_out_pte -> present_bit = 0;
 
-    ku_mmu_pmem.free_head = ku_mmu_pmem.alloc_tail; /* free list의 head가 alloc list의 tail 값이 됨 */
-    ku_mmu_pmem.alloc_tail = ku_mmu_pmem.free_and_alloc[ku_mmu_pmem.alloc_tail].next_index;
+    /* swap out된 pte 값 변경 */
+    ku_pte *swap_out_pte;
+    if (!swap_out_pte)
+        return -1;
+    swap_out_pte->PFN = (swap_dest & PTE_MASK) >> 1;
+    swap_out_pte->unused_bit = swap_dest & 0x1;
+    swap_out_pte->present_bit = 0;
+
     return 0;
 }
 
 /**
- * @brief Swap in accessed pte.
+ * @brief Swap in accessed pte to physical memory.
  * @param accesed_pte PTE wants to swap in to pysical memory.
  * @return 0 on success, -1 on error.
  */
-int swap_in(ku_pte *accesed_pte) {
-    unsigned int take_back = ((accesed_pte -> PFN)<<1) + accesed_pte -> unused_bit;
-    node* taking_node = &ku_mmu_swap.free_and_alloc[take_back];
-    taking_node -> rmap = NULL; // free 된 swap을 free list에 insert
-    ku_mmu_swap.free_and_alloc[taking_node -> prev_index].next_index = taking_node -> next_index;
-    ku_mmu_swap.free_head = take_back;
-    if(map_on_pmem(accesed_pte)) return -1;
+int swap_in(ku_pte *accesed_pte)
+{
+    unsigned int take_back = ((accesed_pte->PFN) << 1) + accesed_pte->unused_bit;
+    node *taking_node;
+    taking_node->rmap = NULL; // free 된 swap을 free list에 insert
+    if (map_on_pmem(accesed_pte))
+        return -1;
     return 0;
 }
 
 /**
- * @brief Map accessed pte to pysical memory.
+ * @brief Map accessed PTE to pysical memory.
  * @param accesed_pte PTE wants to map.
  * @return 0 on success, -1 on error.
  */
-int map_on_pmem(ku_pte *accesed_pte) {
-    if(!ku_mmu_pmem.free_head)
-        if(swap_out()) return -1;
-    accesed_pte -> PFN = ku_mmu_pmem.free_head; // accessed pte에게 PFN 할당
-    accesed_pte -> present_bit = 1;
-    accesed_pte -> unused_bit = 0;
-    if(ku_mmu_pmem.alloc_tail == 0) {
-        ku_mmu_pmem.alloc_head = 1;
-        ku_mmu_pmem.alloc_tail = 1;
-    } else {
-        ku_mmu_pmem.alloc_head = ku_mmu_pmem.free_and_alloc[ku_mmu_pmem.alloc_head].next_index;
-    }
-    ku_mmu_pmem.free_and_alloc[ku_mmu_pmem.alloc_head].rmap = accesed_pte; // 할당된 pte reverse mapping
-    ku_mmu_pmem.free_head = ku_mmu_pmem.free_and_alloc[ku_mmu_pmem.free_head].next_index; // free head 변경
+int map_on_pmem(ku_pte *accesed_pte)
+{
+    if (1)
+        if (swap_out())
+            return -1;
+    accesed_pte->PFN; // accessed pte에게 PFN 할당
+    accesed_pte->present_bit = 1;
+    accesed_pte->unused_bit = 0;
+
+    /* 할당된 pte reverse mapping */
     return 0;
 }
 
-void init_space(space* input, unsigned int space_size) {
-    input -> alloc_area = (char *)malloc(space_size);
-    unsigned int num_of_page_in_input = space_size/PAGE_SIZE;
-    input -> free_and_alloc = (node *)malloc(space_size*sizeof(node));
-    input -> free_and_alloc[1].prev_index = 0;
-    for(int i = 1; i < num_of_page_in_input - 1; i++) {
+/**
+ * @brief Initalize memory space to use in KU_MMU.
+ *
+ * @param input Space name.
+ * @param space_size Demanding space size.
+ */
+void init_space(space *input, unsigned int space_size)
+{
+    input->alloc_area = (char *)malloc(space_size);
+    unsigned int num_of_page_in_input = space_size / PAGE_SIZE;
+    for (int i = 1; i < num_of_page_in_input - 1; i++)
+    {
         // input -> free_and_alloc[i].pid = 0;
-        input -> free_and_alloc[i].next_index = i+1;
-        input -> free_and_alloc[i+1].prev_index = i;
     }
     // input -> free_and_alloc[num_of_page_in_input].pid = 0;
-    input -> free_and_alloc[num_of_page_in_input-1].next_index = 0;
-    input -> free_head = 1;
-    input -> free_tail = 0;
-    input -> alloc_head = 0;
-    input -> alloc_tail = 0;
 }
 
 // 메모리와 스왑 공간 동적할당
-void *ku_mmu_init(unsigned int pmem_size, unsigned int swap_size) {
+void *ku_mmu_init(unsigned int pmem_size, unsigned int swap_size)
+{
     init_space(&ku_mmu_pmem, pmem_size);
     init_space(&ku_mmu_swap, swap_size);
-    PCB_list_ptr -> head = NULL;
-    if(ku_mmu_pmem.alloc_area == NULL) return 0;
-    else return ku_mmu_pmem.alloc_area;
+    PCB_list_ptr->head = NULL;
+    if (ku_mmu_pmem.alloc_area == NULL)
+        return 0;
+    else
+        return ku_mmu_pmem.alloc_area;
 }
 
 // 새로운 프로세스 생성 but 실제로 fork()를 통해 생성하는 것이 아닌 새로운 PCB만 생성
@@ -185,31 +186,38 @@ void *ku_mmu_init(unsigned int pmem_size, unsigned int swap_size) {
 // 전에 생성된 프로세스라면 해당 프로세스의 PDBR 반환
 // 프로세스당 256B Page Table 생성
 // PCB를 PCB list에 insert
-int ku_run_proc(char fpid, void **ku_cr3) {
+int ku_run_proc(char fpid, void **ku_cr3)
+{
     tempPCB = searchPCB(fpid);
-    if(!tempPCB) { // PCBlist에 fpid에 해당하는 PCB가 없을 경우
+    if (!tempPCB)
+    { // PCBlist에 fpid에 해당하는 PCB가 없을 경우
         tempPCB = (PCB *)malloc(sizeof(PCB));
-        tempPCB -> PTBR = (ku_pte *)calloc(64, sizeof(ku_pte));
-        tempPCB -> pid = fpid;
+        tempPCB->PTBR = (ku_pte *)calloc(64, sizeof(ku_pte));
+        tempPCB->pid = fpid;
         insertPCB(tempPCB);
     }
-    *ku_cr3 = tempPCB -> PTBR;
+    *ku_cr3 = tempPCB->PTBR;
     return 0;
 }
-
 
 // PTE did not mapped
 // swaped out?? or map new PTE?? Both case needs to consider free space in swap and pmem
 // 해당 함수가 호출되면 이후 ku_trav()가 실행될 때 address translation 성공 필요
 // ! swap 계열의 함수 작동 실패시 -1 반환 후 ku_page_fault 종료해야함
-int ku_page_fault(char pid, char va) {
-    ku_pte *page_table = tempPCB -> PTBR; // PCB를 통해 
-	char pte_offset = (va & PFN_MASK) >> 2;
+int ku_page_fault(char pid, char va)
+{
+    ku_pte *page_table = tempPCB->PTBR; // PCB를 통해
+    char pte_offset = (va & PFN_MASK) >> 2;
     ku_pte *target_pte = page_table + pte_offset; // 여기서 3시간 날림 대체 char *랑 ku_pte * 크기가 왜 다를까?
-    if(target_pte -> PFN == 0x0 && target_pte -> unused_bit == 0x0) {
-        if(map_on_pmem(target_pte)) return -1; // TODO pbit이 아닌 ubit 값이 갱신됨
-    } else {
-        if(swap_in(target_pte)) return -1; 
+    if (target_pte->PFN == 0x0 && target_pte->unused_bit == 0x0)
+    {
+        if (map_on_pmem(target_pte))
+            return -1; // TODO pbit이 아닌 ubit 값이 갱신됨
+    }
+    else
+    {
+        if (swap_in(target_pte))
+            return -1;
     }
     return 0;
 }
